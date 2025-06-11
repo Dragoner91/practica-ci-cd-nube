86595f9dc573990d8ae527c0a6cd7d26906e4b66
Dragoner91 <Dragoner919@hotmail.com>
2025-06-10 20:45:12 -0400

    update greeting message in root route to Spanish

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World from Docker and Jenkins!');
+  res.send('Hola, jenkins');
 });
 
 app.listen(port, () => {
